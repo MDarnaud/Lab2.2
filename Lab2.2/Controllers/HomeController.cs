effac98f778e710fc4bf088649a2fcd6e8c046fd
Megane <38261962+MDarnaud@users.noreply.github.com>
2019-08-29 13:22:47 -0400

    Add project files.

---
+++ Lab2.2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Index.";
+
+            return View();
+        }
+
+        public ActionResult Semester5()
+        {
+            ViewBag.Message = "Semester 5.";
+
+            return View();
+        }
+
+        public ActionResult Testimonial()
+        {
+            ViewBag.Message = "Student quotes.";
+
+            return View();
+        }
+    }
+}
